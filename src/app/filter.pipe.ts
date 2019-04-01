1a444e176d641b520392cfc4d3871ecbf6901027
abdoulrhman <abdoulrhman_salah@hotmail.com>
2019-04-01 15:17:23 +0200

    Fisrt Push

---
+++ src/app/filter.pipe.ts
@@ -0,0 +1,50 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(movies: any): any {
+    let a = [];
+    // Parse the serialized data back into an aray of objects
+    a = JSON.parse(localStorage.getItem('session'));
+
+    if (!movies || !a) {
+      return movies;
+    }
+    console.log(movies.filter((movie)=>{ 
+      a.forEach((id)=>{
+        console.log(id);
+        console.log(movie);
+        
+        if(movie.id != id){
+          return movie;
+        }
+      })
+      }));
+    
+    // return movies.filter((movie)=>{ 
+    //   let isFiltered = false;
+    //   a.forEach((id)=>{
+    //     console.log(id);
+    //     console.log(movie);
+        
+    //     if(movie.id != id){
+    //       isFiltered = true;
+    //       return;
+    //     }
+    //   })
+    //   if(isFiltered) { return movie; }
+    //   });
+    a.forEach(id => {
+      movies.forEach((movie,i) => {
+        if(movie.id == id){
+          movies.splice(i, 1); 
+        }
+      });
+    });
+    return movies;
+  }
+
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../movie.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  popular_movies: any;
+  upcoming_movies: any;
+  search_result: any;
+  movie: any;
+
+
+  constructor(public movieService: MovieService) {
+
+
+
+    // get upcoming movies
+    this.movieService.getUpcomingMovies().subscribe(data => {
+      this.upcoming_movies = data['results'];
+      // console.log(this.upcoming_movies);
+    });
+
+    // get popular movies
+    this.movieService.getPopularMovies().subscribe(data => {
+      this.popular_movies = data['results'];
+      // console.log(this.popular_movies);
+    });
+
+  }
+
+  // get search results of movies
+  searchMovies() {
+    this.movieService.searchMovie(this.movie).subscribe(data => {
+      this.search_result = data['results'];
+      // console.log(this.search_result);
+    });
+  }
+
+
+  addToDelteList(id: any,arr:any, index:any){
+
+    let r = confirm("You Are About To Delete The Movie ");
+
+    
+    if (r == true) {
+      let a = [];
+      // Parse the serialized data back into an aray of objects
+      if(JSON.parse(localStorage.getItem('session'))){
+        a = JSON.parse(localStorage.getItem('session'));
+      }
+  
+      // Push the new data (whether it be an object or anything else) onto the array
+      a.push(id);
+      // Alert the array value
+      // alert(a);  // Should be something like [Object array]
+      // Re-serialize the array back into a string and store it in localStorage
+      localStorage.setItem('session', JSON.stringify(a));
+  
+      arr.splice(index,1);
+    } else {
+      console.log("UNdo");
+      
+    }
+    
+  }
+
+  clearAll(){
+    localStorage.clear();
+    location.reload();
+  }
+
+  ngOnInit() {
+  }
+
+}

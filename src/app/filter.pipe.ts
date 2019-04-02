45083678432d8518f0745beb884f39352a92c36b
abdoulrhman <abdoulrhman_salah@hotmail.com>
2019-04-02 14:40:58 +0200

    created Pipe To Filter the deleted movies

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -1,50 +1,37 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'filter'
+  name: "filter"
 })
 export class FilterPipe implements PipeTransform {
-
   transform(movies: any): any {
     let a = [];
     // Parse the serialized data back into an aray of objects
-    a = JSON.parse(localStorage.getItem('session'));
+    a = JSON.parse(localStorage.getItem("session"));
 
     if (!movies || !a) {
       return movies;
     }
-    console.log(movies.filter((movie)=>{ 
-      a.forEach((id)=>{
-        console.log(id);
-        console.log(movie);
-        
-        if(movie.id != id){
-          return movie;
-        }
+    console.log(
+      movies.filter(movie => {
+        a.forEach(id => {
+          console.log(id);
+          console.log(movie);
+
+          if (movie.id != id) {
+            return movie;
+          }
+        });
       })
-      }));
-    
-    // return movies.filter((movie)=>{ 
-    //   let isFiltered = false;
-    //   a.forEach((id)=>{
-    //     console.log(id);
-    //     console.log(movie);
-        
-    //     if(movie.id != id){
-    //       isFiltered = true;
-    //       return;
-    //     }
-    //   })
-    //   if(isFiltered) { return movie; }
-    //   });
+    );
+
     a.forEach(id => {
-      movies.forEach((movie,i) => {
-        if(movie.id == id){
-          movies.splice(i, 1); 
+      movies.forEach((movie, i) => {
+        if (movie.id == id) {
+          movies.splice(i, 1);
         }
       });
     });
     return movies;
   }
-
 }
